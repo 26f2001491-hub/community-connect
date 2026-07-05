3712521e1a9901a53c0cd903990d9be37007a8df
Anurag Kumar <26f2001491@ds.study.iitm.ac.in>
2026-07-05 16:02:17 +0530

    remove all secrets from config

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,9 +6,9 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
     DB_ECHO: bool = False
-    TWILIO_ACCOUNT_SID: str = "AC65f7b5a75f90c85e7f9a16205c9b1a6d"
-    TWILIO_AUTH_TOKEN: str = "1703973fe7d08e1db6bb74a3d4b89266"
-    TWILIO_VERIFY_SID: str = "VA324ece2ab74b1a4ddfb02c3bee2f0d07"
+    TWILIO_ACCOUNT_SID: str = ""
+    TWILIO_AUTH_TOKEN: str = ""
+    TWILIO_VERIFY_SID: str = ""
 
 
 settings = Settings()
